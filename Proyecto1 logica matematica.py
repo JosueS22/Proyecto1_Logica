71f3cc6563c9ca68f9f9535f89fee133d3ab898a
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-08-13 13:30:15 -0600

    finished

--- Proyecto1 logica matematica.py
+++ Proyecto1 logica matematica.py
@@ -9,10 +9,10 @@
 # Cierres Transitivos de un grafo
 
 # Se define la matriz de adyacencia del grafo 
-graph = [[0, 0, 0, 1],
-        [0, 0, 1, 1],
+graph = [[0, 0, 1, 1],
+        [1, 0, 0, 0],
         [0, 1, 0, 0],
-        [0, 1, 0, 1]]
+        [0, 0, 0, 0]]
 
 print("\nMatriz de adyacencia del grafo")
 for i in graph:
@@ -31,24 +31,28 @@ def warshall_algorithm(v, graph):
 # de vertices, la matriz de adyacencia (dos veces) y una matriz inicial 
 # que por default va a tener 0 en todas sus entradas
 def boolean_matrix(v, graph1, graph2, initial_graph = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]):
-    actual_graph = initial_graph
+    current_graph = initial_graph
     for i in range(v):
         for j in range(v):
             for k in range(v):
-                actual_graph[i][j] += graph1[i][k] *graph2[k][j]
-                if actual_graph[i][j] > 1:
-                    actual_graph[i][j] = 1
-    if(actual_graph == graph1):
-        return actual_graph
+                current_graph[i][j] += graph1[i][k] *graph2[k][j]
+                if current_graph[i][j] > 1:
+                    current_graph[i][j] = 1
+    if(current_graph == graph1):
+        return current_graph
     else:
-        boolean_matrix(v, actual_graph, graph1, actual_graph)
+        boolean_matrix(v, current_graph, graph1, current_graph)
 
 transitiveClosure_graph = warshall_algorithm(len(graph), graph)
 print("\nCierre transitivo utilizando el algoritmo de Warshall:")
 for i in transitiveClosure_graph:
     print(i)
 
-transitiveClosure_graph = boolean_matrix(len(graph), graph, graph)
+transitiveClosure_graph2 = boolean_matrix(len(graph), graph, graph)
 print("\nCierre transitivo utilizando matrices booleanas:")
-for r in transitiveClosure_graph:
+for r in transitiveClosure_graph2:
     print(r)
+
+# Referencias
+# https://www.tutorialspoint.com/Transitive-closure-of-a-Graph#:~:text=Transitive%20Closure%20it%20the%20reachability,matrix%20is%20the%20Boolean%20type.
+# http://repositori.uji.es/xmlui/bitstream/handle/10234/119829/tema11.pdf?sequence=1
